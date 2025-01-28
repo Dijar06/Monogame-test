84d36f42632917f336b0b40cbc7b46f56ad1789d
D7a8 <>
2025-01-28 16:19:49 +0100

    lektion 1

---
+++ Game1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame_test;
+
+public class Game1 : Game
+{
+    Texture2D xwing;
+    Vector2 xwingPos = new Vector2(100,300);
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+        xwing = Content.Load<Texture2D>("xwing");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+        KeyboardState kstate = Keyboard.GetState();
+        if(kstate.IsKeyDown(Keys.Right)){
+            xwingPos.X++;
+        }
+        if(kstate.IsKeyDown(Keys.Left)){
+            xwingPos.X--;
+        }
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(xwing, xwingPos, Color.White);
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
